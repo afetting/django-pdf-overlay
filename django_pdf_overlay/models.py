c59bef54755da84d4ef80bd212023f79619fa920
Andrew <afetting@mjfinchworks.com>
2022-10-18 12:55:44 -0500

    Added Image Field

--- django_pdf_overlay/models.py
+++ django_pdf_overlay/models.py
@@ -13,7 +13,10 @@ from django.utils.deconstruct import deconstructible
 from django.utils.encoding import force_str
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 from PIL import Image
+pdfmetrics.registerFont(TTFont('Calibri', './media/fonts/calibri-font-family/calibri-regular.ttf'))
 
 from . import app_settings, utils, validators
 from .commands import get_commands
--- print_test/views.py
+++ print_test/views.py
@@ -8,5 +8,5 @@ from django_pdf_overlay.models import Document
 class PrintTestView(View):
     def get(self, request, *args, **kwargs):
         doc = Document.objects.get(name='test')
-        doc.render_pages(card_data={'test_image': 'media/blank-profile-picture.jpg'})
+        doc.render_pages(card_data={'test': 'TEST', 'test_image': 'media/student_photo/7f58f6a2-cc9d-4b9e-9415-8ce4c9156747.jpg'})
         return doc.render_as_response()
